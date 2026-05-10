46bd19bbe1a9d3d90bf407b776c6a1049befab65
Raúl Romero <raulromero@paucasesnovescifp.cat>
2026-05-10 20:51:16 +0200

    feat: crear classe Tasca a models.js amb els camps id, titol, descripcio, data, categoria, prioritat i realitazda

--- js/models.js
+++ js/models.js
@@ -1,6 +1,18 @@
 export class Categoria {
   constructor(nom, color) {
-    this.nom   = nom;
+    this.nom = nom;
     this.color = color;
+  }
+}
+
+export class Tasca {
+  constructor({ id, titol, descripcio, data, categoria, prioritat, realitzada = false }) {
+    this.id = id;
+    this.titol = titol;
+    this.descripcio = descripcio;
+    this.data = data;
+    this.categoria = categoria; 
+    this.prioritat = prioritat; 
+    this.realitzada = realitzada;
   }
 }
