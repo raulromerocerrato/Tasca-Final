3cc59cfd96fc44c330f65f16e2c12d7d279395a9
Raúl Romero Cerrato <raulromero@paucasesnovescifp.cat>
2026-05-15 09:12:45 +0200

    feat: afegides totes les variables agafades amb el ID

--- js/form-tasca.js
+++ js/form-tasca.js
@@ -1,2 +1,11 @@
 import { afegirTasca, getCategories, getCategoriaByNom } from './storage.js';
 import { Tasca } from './models.js';
+
+const form = document.getElementById('form-tasca');
+const inputTitol = document.getElementById('titol');
+const inputDescripcio = document.getElementById('descripcio');
+const inputData = document.getElementById('data');
+const selectCategoria = document.getElementById('categoria');
+const selectPrioritat = document.getElementById('prioritat');
+const colorPreview = document.getElementById('cat-color-preview');
+const btnSubmit = form?.querySelector('button[type="submit"]');
