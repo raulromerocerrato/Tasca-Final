1231c5eccfed021db96575504c02e1cdb488879b
Raúl Romero <raulromero@paucasesnovescifp.cat>
2026-05-17 20:51:01 +0200

    feat: creada funcio prioritatClass() per saber quina prioritat té a categories.js

--- js/categories.js
+++ js/categories.js
@@ -1,2 +1,11 @@
 import {getCategories, afegirCategoria, eliminarCategoria} from './storage.js';
 import { Categoria } from './models.js';
+
+function prioritatClass(prioritat) {
+  const mapa = {
+    'Alta': 'prioritat-alta',
+    'Mitjana': 'prioritat-mitjana',
+    'Baixa': 'prioritat-baixa',
+  };
+  return mapa[prioritat] || '';
+}
