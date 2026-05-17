c00d127acab2c79a7e6d52b9ecaf75ed34c2a0e3
Raúl Romero <raulromero@paucasesnovescifp.cat>
2026-05-17 22:53:11 +0200

    feat: afegits constants a index.js

--- js/index.js
+++ js/index.js
@@ -1,6 +1,12 @@
 import {getTasques, eliminarTasca, marcarRealitzada, afegirTasca} from './storage.js';
 import { Tasca } from './models.js';
 
+const pendentsContenidor = document.querySelector('.tasques-pendents');
+const acabadesContenidor = document.querySelector('.tasques-acabades-contenidor');
+const inputFitxer = document.getElementById('input-fitxer');
+const botoPujar = document.getElementById('btn-pujar');
+const labelFitxer = document.getElementById('label-fitxer');
+
 function prioritatClass(prioritat){
   const mapa = {
     'Alta': 'prioritat-alta',
