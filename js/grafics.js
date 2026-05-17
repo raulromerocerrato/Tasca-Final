4ffa6bd899baf4850ce20155fd478a370535e812
Raúl Romero <raulromero@paucasesnovescifp.cat>
2026-05-17 22:49:33 +0200

    feat: afegit agruparPerMes() per agrupa rtasques per mesos

--- js/grafics.js
+++ js/grafics.js
@@ -1,1 +1,12 @@
 import { getTasquesAcabades } from './storage.js';
+
+function agruparPerMes(tasques) {
+  const comptes = {};
+  tasques.forEach(tasca => {
+    const mes = tasca.data ? tasca.data.slice(0, 7) : 'Desconegut';
+    comptes[mes] = (comptes[mes] || 0) + 1;
+  });
+  const labels = Object.keys(comptes).sort();
+  const valors  = labels.map(m => comptes[m]);
+  return { labels, valors };
+}
