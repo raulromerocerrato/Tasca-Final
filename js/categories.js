d63ed846550a300528a1188e14787fe3a8b62c48
Raúl Romero <raulromero@paucasesnovescifp.cat>
2026-05-17 21:15:39 +0200

    feat: creat el render de categoories a categories.js

--- js/categories.js
+++ js/categories.js
@@ -5,3 +5,14 @@ const inputNom = document.getElementById('input-nom-cat');
 const inputColor = document.getElementById('input-color-cat');
 const botoAfegir = document.getElementById('btn-afegir');
 const llistaCat = document.getElementById('llista-categories');
+
+function renderCategories() {
+  const categories = getCategories();
+  llistaCat.innerHTML = '';
+  if (categories.length === 0) {
+    llistaCat.innerHTML = '<p class="buit">No hi ha categories definides.</p>';
+    return;
+  }
+
+  categories.forEach(categoria => renderCategoriaItem(categoria));
+}
